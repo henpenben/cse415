19098b4c6aa4f3e919787b102e41ebf6023411fd
henpenben <henryhow@uw.edu>
2021-04-14 19:33:15 -0700

    restructured directories

---
+++ hw2/Farmer_Fox.py
@@ -0,0 +1,19 @@
+'''Farmer_Fox.py
+by Henry Hough
+UWNetID: henryhow
+Student number: 1866071
+
+Assignment 2, in CSE 415, Spring 2021.
+
+This file contains my problem formulation for the problem of
+the Farmer, Fox, Chicken, and Grain.
+'''
+
+# Put your formulation of the Farmer-Fox-Chicken-and-Grain problem here.
+# Be sure your name, uwnetid, and 7-digit student number are given above in
+# the format shown.
+PROBLEM_NAME = "Farmer-Fox-Chicken-and-Grain"
+
+
+class State:
+    def __init__(self, d)
