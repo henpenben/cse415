6ec69f15b64518a391c0ae94d91fcc5cea6b1e1f
henpenben <henryhow@uw.edu>
2021-04-14 23:37:55 -0700

    modified DFS to be a BFS

--- hw2/ItrBFS.py
+++ hw2/ItrBFS.py
@@ -33,8 +33,102 @@ class ItrBFS:
         print("\nWelcome to ItrBFS")
 
     def runBFS(self):
-        # Comment out the line below when this function is implemented.
-        raise NotImplementedError
+        initial_state = self.Problem.CREATE_INITIAL_STATE()
+        print("Initial State:")
+        print(initial_state)
+
+        self.COUNT = 0
+        self.MAX_OPEN_LENGTH = 0
+        self.BACKLINKS = {}
+
+        self.IterativeBFS(initial_state)
+        print(f"Number of states expanded: {self.COUNT}")
+        print(f"Maximum length of the open list: {self.MAX_OPEN_LENGTH}")
+
+    def IterativeBFS(self, initial_state):
+        """This is the actual algorithm"""
+        # STEP 1. Put the start state on a list OPEN
+        OPEN = [initial_state]
+        CLOSED = []
+        self.BACKLINKS[initial_state] = None
+
+        # STEP 2. If OPEN is empty, output “DONE” and stop.
+        while OPEN != []:
+            report(OPEN, CLOSED, self.COUNT)
+            if len(OPEN) > self.MAX_OPEN_LENGTH:
+                self.MAX_OPEN_LENGTH = len(OPEN)
+
+            # STEP 3. Select the first state on OPEN and call it S.
+            #         Delete S from OPEN.
+            #         Put S on CLOSED.
+            #         If S is a goal state, output its description
+            S = OPEN.pop(0)
+            CLOSED.append(S)
+
+            if self.Problem.GOAL_TEST(S):
+                print(self.Problem.GOAL_MESSAGE_FUNCTION(S))
+                self.PATH = [str(state) for state in self.backtrace(S)]
+                self.PATH_LENGTH = len(self.PATH) - 1
+                print(f"Length of solution path found: {self.PATH_LENGTH} edges")
+                return
+            self.COUNT += 1
+
+            # STEP 4. Generate the list L of successors of S and delete
+            #         from L those states already appearing on CLOSED.
+            L = []
+            for op in self.Problem.OPERATORS:
+                if op.is_applicable(S):
+                    new_state = op.apply(S)
+                    if new_state not in OPEN and new_state not in CLOSED:
+                        L.append(new_state)
+                        self.BACKLINKS[new_state] = S
+
+            # STEP 5. Delete from L any members of OPEN that occur on L.
+            #         Insert all members of L at the end of OPEN.
+            for s2 in L:
+                for i in range(len(OPEN)):
+                    if s2 == OPEN[i]:
+                        L.remove(s2)
+                        break
+            for i in range(len(L)):
+                OPEN.append(L[i])
+            print_state_list("OPEN", OPEN)
+
+    # STEP 6. Go to Step 2.
+
+    def backtrace(self, S):
+        path = []
+        while S:
+            path.append(S)
+            S = self.BACKLINKS[S]
+        path.reverse()
+        print("Solution path: ")
+        for s in path:
+            print(s)
+        return path
+
+
+def print_state_list(lst_name, lst):
+    """
+    Prints the states in lst with name lst_name
+    """
+    print(f"{lst_name} is now: ", end='')
+    for s in lst[:-1]:
+        print(str(s), end=', ')
+    print(str(lst[-1]))
+
+
+def report(opn, closed, count):
+    """
+    Reports the current statistics:
+    Length of open list
+    Length of closed list
+    Number of states expanded
+    """
+    print(f"len(OPEN)= {len(opn)}", end='; ')
+    print(f"len(CLOSED)= {len(closed)}", end='; ')
+    print(f"COUNT = {count}")
+
 
 
 if __name__ == '__main__':
