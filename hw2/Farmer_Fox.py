b7558268ee529e53aa6324e498dedbc6f13cd2a0
Henry <henryhow@uw.edu>
2021-04-14 22:05:49 -0700

    Farmer_Fox is now working

--- hw2/Farmer_Fox.py
+++ hw2/Farmer_Fox.py
@@ -12,7 +12,11 @@ the Farmer, Fox, Chicken, and Grain.
 # Put your formulation of the Farmer-Fox-Chicken-and-Grain problem here.
 # Be sure your name, uwnetid, and 7-digit student number are given above in
 # the format shown.
-PROBLEM_NAME = "Farmer-Fox-Chicken-and-Grain"
+PROBLEM_NAME = "Farmer-Fox-Chicken-Grain"
+PROBLEM_VERSION = "0.1"
+PROBLEM_AUTHORS = ['H. Hough']
+PROBLEM_CREATION_DATE = "14-APR-2021"
+PROBLEM_DESC = "A basic formulation of the Ffcg problem using native Python3.9"
 
 #<COMMON_CODE>
 L = 'left'
@@ -37,13 +41,13 @@ class State:
 
     # returns a string representation of current state
     def __str__(self):
-      output = ""
+      output = "\n"
       output += "Left side:\n"
       for entity in self.s[L]:
         output += entity + "\n"
-      output += "\nRight side:\n"
+      output += "\n            Right side:\n"
       for entity in self.s[R]:
-        output += entity + "\n"
+        output += "            " + entity + "\n"
       return output
 
     # returns a hash of the current state
@@ -53,9 +57,90 @@ class State:
     # returns a copy of current state
     def copy(self):
       copy = State({})
-      copy.s[L].clear()
-      for entity in self.s[L]:
-        copy.s[L].append(entity)
-      for entity in self.s[R]:
-        copy.s[R].append(entity)
+      for direction in [L, R]:
+        copy.s[direction] = self.s[direction].copy()
       return copy
+
+    # returns whether a move is legal
+    def can_move(self, move):
+      mvC = move.copy()
+      #1 Farmer must be on every boat trip
+      if F not in mvC: return False
+
+      # Determine what side the farmer is moving from
+      side = ''
+      if F in self.s[L]: side = L
+      else: side = R
+
+      #2 Only zero or one item(s) per trip
+      mvC.remove(F)
+      if len(mvC) != 1: return False
+
+      #2 Farmer can only take things from the side he is currently on
+      passenger = ''
+      afterMove = self.s[side].copy()
+      if mvC[0] is not None:
+        passenger = mvC[0]
+        if passenger not in self.s[side]: return False
+        afterMove.remove(passenger) # Remove the passenger for next tests
+
+      #4 Fox cannot be left alone with chicken
+      if X in afterMove and C in afterMove: return False
+      #5 Chicken cannot be left alone with grain
+      if C in afterMove and G in afterMove: return False
+
+      return True
+
+    def move(self, move):
+      mvC = move.copy()
+      side = ['',''] # side[0] is the side being moved towards
+      if F in self.s[L]:
+        side = [L,R]
+      else:
+        side = [R,L]
+
+      mvC.remove(F)
+
+      # move the farmer
+      new = self.copy()
+      new.s[side[0]].remove(F)
+      new.s[side[1]].append(F)
+
+      # move the passenger, if applicable
+      if mvC[0] is not None:
+        new.s[side[1]].append(mvC[0])
+        new.s[side[0]].remove(mvC[0])
+      return new
+
+def goal_test(state):
+  return len(state.s[L]) == 0
+
+def goal_message(s):
+  return "Congratulations! You got everything across the river without things eating eachother!"
+
+class Operator:
+  def __init__(self, name, precond, state_transf):
+    self.name = name
+    self.precond = precond
+    self.state_transf = state_transf
+
+  def is_applicable(self, s):
+    return self.precond(s)
+
+  def apply(self, s):
+    return self.state_transf(s)
+
+combinations = [[F,None], [F,X], [F,C], [F,G]]
+
+OPERATORS = [
+  Operator(
+  "Cross river with " + str(mv[0]) + " and the " + str(mv[1]),
+  lambda s, mv=mv: s.can_move(mv),
+  lambda s, mv=mv: s.move(mv))
+  for mv in combinations]
+
+CREATE_INITIAL_STATE = lambda: State()
+
+GOAL_TEST = lambda s: goal_test(s)
+
+GOAL_MESSAGE_FUNCTION = lambda s: goal_message(s)
